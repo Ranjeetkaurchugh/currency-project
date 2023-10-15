d59e1e10d3fa9b97157fb6eadcdde965101ede27
Ranjeet kaur chugh <102470805+Ranjeetkaurchugh@users.noreply.github.com>
2023-10-15 09:46:06 +0000

    debugging

--- Currency-project/src/App.jsx
+++ Currency-project/src/App.jsx
@@ -1,16 +1,82 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { InputBox } from './components/Index'
+import usecurrencyinfo from './hooks/usecurrencyinfo'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+const [amount,SetAmount]=useState(0)
+const[From,SetFrom]=useState("usd")
+const[To,setTo]=useState("inr")
+const[Converted,SetConverted] =useState(0)
+const currencyinfo=usecurrencyinfo(From) //our customize hook calling
+//y humko naa jo data milla hai vo objects ki form hai hai i.e key value pairs mein isliye humne objects ki key li hai hai  to get the value.
+const Option = Object.keys(currencyinfo)
+const swap=()=>{
+  SetFrom(To)
+  setTo(From)
+  SetAmount(Converted)
+  SetConverted(amount)
 
-  return (
-    <>
-     <h1 className='text-400 text-center text-white'>Currency App</h1>
-    </>
-  )
+  
+}
+const convert =()=>{SetConverted=(amount*currencyinfo[To])}
+
+
+ return (
+        <div
+            className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+            style={{
+                backgroundImage: `url('/workspaces/currency-project/Currency-project/src/sl_020622_4930_13.jpg')`,
+            }}
+        >
+            <div className="w-full">
+                <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+                    <form
+                        onSubmit={(e) => {
+                            e.preventDefault();
+                            convert();
+                           
+                        }}
+                    >
+                        <div className="w-full mb-1">
+                            <InputBox
+                                label="From"
+                                amount={amount}
+                                currencyOptions={Option}
+                                oncurrencychange={(currency)=>SetAmount(amount)}
+                                selectcurrency={from}
+                                
+                            />
+                        </div>
+                        <div className="relative w-full h-0.5">
+                            <button
+                                type="button"
+                                className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                                onClick={swap}
+                            >
+                                swap
+                            </button>
+                        </div>
+                        <div className="w-full mt-1 mb-4">
+                            <InputBox
+                                label="To"
+                                amount={amount}
+                                currencyOptions={Option}
+                                onCurrencyChange={(currency) => setTo(currency)}
+                            selectCurrency={From}
+                            amountDisable
+
+                                
+                            />
+                        </div>
+                        <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
+                        Convert {From.toUpperCase()} to {To.toUpperCase()}
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App
--- Currency-project/src/components/InputBox.jsx
+++ Currency-project/src/components/InputBox.jsx
@@ -1,17 +1,63 @@
-import React from "react";
-function InputBox(){
-    label,
+import React ,{useId} from "react";
+function InputBox({ label,
     amount,
     OnAmountChange,
     OnCurrencyChange,
-    
+    currenctChangeOptions=[],//user jo option degaa vo toh hai hi but saath mein ek empty array diy a hai incase vo naa de toh alteast 
+    //app crash naa ho.
+    selectCurrency="usd",
+    amountDisable="false",
+    currencyDisable="false",
+    className="",}){
+        const id= useId()
+
+   
+
+
 
     return (
+        <div className={`bg-white p-3 rounded-lg text-sm flex `}>
+        <div className="w-1/2">
+        <label htmlFor={id} className="text-black/40  mb-2  inline-block"> {label}
+        </label>
+        <input
+                    id={id}
+                    className="outline-none w-full bg-transparent py-1.5"
+                    type="number"
+                    placeholder="Amount"
+                    disabled={amountDisable}
+                    value={OnAmountChange}
+                    onChange={(e)=>OnAmountChange && OnAmountChange(Number(e.target.value))} //js kabhi kabhi string mein amount le leti hai isliye phele hi change krlete hai Nuumber mein bss isvjh se number lgya hai.
+
+                />
+           
+            <div className="w-1/2 flex flex-wrap justify-end text-right">
+                <p className="text-black/40 mb-2 w-full">Currency Type</p>
+                <select
+                    className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+                    value={selectCurrency}
+                    onChange={(e)=>OnCurrencyChange && OnCurrencyChange(e.target.value)}
+                    disabled={currencyDisable}
+
+                    
+                >
+                    
+                       {currenctChangeOptions.map((currency)=>( 
+                       <option value={currency}>
+                           {currency}
+                        </option>))}
+                
+                </select>
+                </div>
+
+        </div>
+        </div>
+        
 
 
 
 
-    )
+    );
     
 }
 
